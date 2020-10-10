2bcef83cf11daf040a37e647de1bfda4a324418e
Niki Rahmadi Wiharto <nikirahmadiwiharto@gmail.com>
2020-10-11 04:33:04 +0700

    clone RMOL SUMUT

--- lib/main.dart
+++ lib/main.dart
@@ -16,7 +16,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'RMOL JATIM',
+      title: 'RMOL SUMUT',
       routes: {
         '/home': (context) => HomePage(),
       },
--- lib/ui/page/news/news_detail.dart
+++ lib/ui/page/news/news_detail.dart
@@ -138,7 +138,7 @@ class _NewsDetailState extends State<NewsDetail> {
                               borderRadius: BorderRadius.circular(50),
                               image: DecorationImage(
                                 fit: BoxFit.cover,
-                                image: NetworkImage("http://rmoljatim.id/assets/img/user.png"),
+                                image: NetworkImage("http://rmolsumut.id/assets/img/user.png"),
                               ),
                             ),
                           ),
