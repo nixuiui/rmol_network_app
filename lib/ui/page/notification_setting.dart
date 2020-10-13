fa6b382ccd2b9fd10f31bda9bf55e26354584ec0
Niki Rahmadi Wiharto <nikirahmadiwiharto@gmail.com>
2020-10-14 01:46:51 +0700

    convert to rmol jabar

--- lib/ui/page/notification_setting.dart
+++ lib/ui/page/notification_setting.dart
@@ -61,12 +61,12 @@ class _SettingPageState extends State<SettingPage> {
     SharedPreferences prefs = await SharedPreferences.getInstance();
     if(isActive) {
       prefs.setString("subscribeNotif", "subscribed");
-      _firebaseMessaging.subscribeToTopic("rmoljatim_public");
+      _firebaseMessaging.subscribeToTopic("rmoljabar_public");
       print("subscribed");
     }
     else {
       prefs.setString("subscribeNotif", "unsubscribed");
-      _firebaseMessaging.unsubscribeFromTopic("rmoljatim_public");
+      _firebaseMessaging.unsubscribeFromTopic("rmoljabar_public");
       print("unsubscribed");
     }
   }
