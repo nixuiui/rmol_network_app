c4346b6c41f536d3b5f1875a6aa5a73f97b5ed52
Niki Rahmadi Wiharto <nikirahmadiwiharto@gmail.com>
2021-04-10 18:20:54 +0700

    ad

--- lib/helper/ad_manager.dart
+++ lib/helper/ad_manager.dart
@@ -4,7 +4,7 @@ class AdManager {
 
   static String get appId {
     if (Platform.isAndroid) {
-      return "ca-app-pub-7349646297780205~5202545354";
+      return "ca-app-pub-7349646297780205~7295529689";
     } else if (Platform.isIOS) {
       return "<YOUR_IOS_ADMOB_APP_ID>";
     } else {
@@ -14,7 +14,7 @@ class AdManager {
 
   static String get bannerAdUnitId {
     if (Platform.isAndroid) {
-      return "ca-app-pub-7349646297780205/8157201129";
+      return "ca-app-pub-7349646297780205/4134195632";
     } else if (Platform.isIOS) {
       return "<YOUR_IOS_BANNER_AD_UNIT_ID>";
     } else {
