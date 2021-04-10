1ddf1533edef99f9b79f58ee372ac6df7384f056
Niki Rahmadi Wiharto <nikirahmadiwiharto@gmail.com>
2021-04-10 20:07:23 +0700

    update

--- lib/main.dart
+++ lib/main.dart
@@ -16,7 +16,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'RMOL JATIM',
+      title: 'RMOL SUMSEL',
       routes: {
         '/home': (context) => HomePage(),
       },
